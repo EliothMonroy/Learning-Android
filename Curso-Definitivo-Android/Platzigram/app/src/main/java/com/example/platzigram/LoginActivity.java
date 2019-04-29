84e9cb49605fdfec9ea65f7d298481e0fc2772d1
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2019-04-29 00:35:58 -0500

    Agregado hasta bottom bar y fragagmentos

---
+++ Curso-Definitivo-Android/Platzigram/app/src/main/java/com/example/platzigram/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.platzigram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.platzigram.view.CreateAccountActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    void goCreateAccount(View view){
+        Intent intent=new Intent(this, CreateAccountActivity.class);
+        startActivity(intent );
+
+    }
+}
