265c2159e54f49ee8e7fd93879e778acaa0cc0e5
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2019-04-29 21:12:16 -0500

    Added container

--- Curso-Definitivo-Android/Platzigram/app/src/main/java/com/example/platzigram/LoginActivity.java
+++ Curso-Definitivo-Android/Platzigram/app/src/main/java/com/example/platzigram/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.platzigram.view.ContainerActivity;
 import com.example.platzigram.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,4 +21,10 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent );
 
     }
+
+    void login(View view){
+        Intent intent=new Intent(this, ContainerActivity.class);
+        startActivity(intent );
+    }
+
 }
--- Curso-Definitivo-Android/Platzigram/app/src/main/java/com/example/platzigram/view/ContainerActivity.java
+++ Curso-Definitivo-Android/Platzigram/app/src/main/java/com/example/platzigram/view/ContainerActivity.java
@@ -1,15 +1,63 @@
 package com.example.platzigram.view;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.platzigram.R;
+import com.example.platzigram.view.fragment.HomeFragment;
+import com.example.platzigram.view.fragment.ProfileFragment;
+import com.example.platzigram.view.fragment.SearchFragment;
 
 public class ContainerActivity extends AppCompatActivity {
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottombar);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, new HomeFragment())
+                    .commit();
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+
+                new BottomNavigationView.OnNavigationItemSelectedListener(){
+
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        switch (menuItem.getItemId()){
+                            case R.id.home:
+                                fragment=new HomeFragment();
+                                break;
+
+                            case R.id.profile:
+                                fragment=new ProfileFragment();
+                                break;
+
+                            case R.id.search:
+                                fragment=new SearchFragment();
+                                break;
+
+                        }
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .disallowAddToBackStack().commit();
+
+                        return true;
+                    }
+                }
+        );
     }
 }
