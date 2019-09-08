3de704621193c5c5ceded495e18e9d8c4d128a94
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2019-09-08 04:34:12 -0500

    Added push notifications to Platzigram with firebase

--- Curso-Definitivo-Android-2016/Platzigram/app/src/main/java/com/example/platzigram/MyFirebaseMessagingService.java
+++ Curso-Definitivo-Android-2016/Platzigram/app/src/main/java/com/example/platzigram/MyFirebaseMessagingService.java
@@ -1,7 +1,18 @@
 package com.example.platzigram;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 
+import androidx.core.app.NotificationCompat;
+
+import com.example.platzigram.login.view.LoginActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -10,6 +21,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         if(remoteMessage.getNotification().getBody()!=null){
             Log.w("FIREBASE-Notification", remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().getBody());
         }else{
             Log.w("FIREBASE-Notification", "Notificación recibida de firebase");
         }
@@ -18,6 +30,37 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onNewToken(String token) {
         Log.d("FirebaseMessagingSer", "Refreshed token: " + token);
+    }
+    
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        
+        String channelId = getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.heart)
+                        .setContentTitle(getString(R.string.fcm_message))
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+        
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+        
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
     
 }
