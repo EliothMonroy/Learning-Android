26a96d456e7bf5dc83b0a197e3ef9856a8f6c1d4
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2019-09-27 00:00:17 -0500

    Add base code for Android architecture course on Platzi

--- JavaRx/FirstTestKotlin/app/src/main/java/xyz/eliothmonroy/firsttestkotlin/MainActivity.kt
+++ JavaRx/FirstTestKotlin/app/src/main/java/xyz/eliothmonroy/firsttestkotlin/MainActivity.kt
@@ -2,11 +2,21 @@ package xyz.eliothmonroy.firsttestkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.Observable
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Observable.just("Hello reactive world").subscribe {
+            value-> Log.d("TAG",value)
+        }
+        Observable.just("Apple","Orange","Banana").subscribe(
+            {value -> Log.d("TAG","Received: $value")}, //onNext
+            { error->Log.d("TAG","Error: $error")}, //onError
+            {Log.d("TAG","Completed!")} //onComplete
+        )
     }
 }
