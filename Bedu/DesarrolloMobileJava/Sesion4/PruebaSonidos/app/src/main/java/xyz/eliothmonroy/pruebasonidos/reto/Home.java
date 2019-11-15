533af8d7e0204538b75b81a0c24acef9ed3ea60f
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2019-11-14 18:17:07 -0600

    Add refactor reto sesion4

--- Bedu/DesarrolloMobileJava/Sesion4/PruebaSonidos/app/src/main/java/xyz/eliothmonroy/pruebasonidos/reto/Home.java
+++ Bedu/DesarrolloMobileJava/Sesion4/PruebaSonidos/app/src/main/java/xyz/eliothmonroy/pruebasonidos/reto/Home.java
@@ -12,6 +12,7 @@ import xyz.eliothmonroy.pruebasonidos.R;
 public class Home extends AppCompatActivity {
 
     ImageView caballo, cabra, cerdo, gallo, mono, perro, serpiente, tigre;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +27,23 @@ public class Home extends AppCompatActivity {
         perro=findViewById(R.id.perro);
         serpiente=findViewById(R.id.serpiente);
         tigre=findViewById(R.id.tigre);
-
     }
 
     public void hacerSonido(View view) {
+        if(mediaPlayer!=null){
+            mediaPlayer.stop();
+        }
         switch (view.getId()){
-            case R.id.caballo: MediaPlayer.create(this,R.raw.caballo).start(); break;
-            case R.id.cabra: MediaPlayer.create(this,R.raw.cabra).start(); break;
-            case R.id.cerdo: MediaPlayer.create(this,R.raw.cerdo).start(); break;
-            case R.id.gallo: MediaPlayer.create(this,R.raw.gallo).start(); break;
-            case R.id.mono: MediaPlayer.create(this,R.raw.mono).start(); break;
-            case R.id.perro: MediaPlayer.create(this,R.raw.perro).start(); break;
-            case R.id.serpiente: MediaPlayer.create(this,R.raw.serpiente).start(); break;
-            case R.id.tigre: MediaPlayer.create(this,R.raw.tigre).start(); break;
+            case R.id.caballo: mediaPlayer=MediaPlayer.create(this,R.raw.caballo);break;
+            case R.id.cabra: mediaPlayer=MediaPlayer.create(this,R.raw.cabra);break;
+            case R.id.cerdo: mediaPlayer=MediaPlayer.create(this,R.raw.cerdo); break;
+            case R.id.gallo: mediaPlayer=MediaPlayer.create(this,R.raw.gallo); break;
+            case R.id.mono: mediaPlayer=MediaPlayer.create(this,R.raw.mono); break;
+            case R.id.perro: mediaPlayer=MediaPlayer.create(this,R.raw.perro); break;
+            case R.id.serpiente: mediaPlayer=MediaPlayer.create(this,R.raw.serpiente); break;
+            case R.id.tigre: mediaPlayer=MediaPlayer.create(this,R.raw.tigre); break;
             default: break;
         }
+        mediaPlayer.start();
     }
 }
