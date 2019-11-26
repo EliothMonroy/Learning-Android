1bdf0a6064729a42de6d2352099554ce79dcbdf6
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2019-11-25 18:27:04 -0600

    Add reto3 sesión1

--- Bedu/KotlinFundamentals/Sesion1/Sesion-01/Reto-03/src/main.kt
+++ Bedu/KotlinFundamentals/Sesion1/Sesion-01/Reto-03/src/main.kt
@@ -1,17 +1,40 @@
 fun main(args: Array<String>) {
     val vocales = arrayOf("A", "E", "I", "O", "U")
     val letra = "A"
-    var letraBuscada: String
+    val letraBuscada: String
+
+    letraBuscada=if (letra in vocales) letra else "X"
+
+    println("letraBusqueda: $letraBuscada")
 
     /*
      * Solucion Condicionales
      */
 
 
-    var numeros: Array<Int> = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val numeros: Array<Int> = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
     val n1 = 4
 
     /*
      * Soluciones Ciclos
      */
+
+    println("CON FOR")
+    for (num in numeros){
+        if (num==n1){
+            println("Cuatro")
+        }else{
+            println("¿$num es número par?: ${(num%2)==0}")
+        }
+    }
+
+    println("CON FOREACH")
+    numeros.forEach {
+        if (it==n1){
+            println("Cuatro")
+        }else{
+            println("¿$it es número par?: ${(it%2)==0}")
+        }
+    }
+
 }
