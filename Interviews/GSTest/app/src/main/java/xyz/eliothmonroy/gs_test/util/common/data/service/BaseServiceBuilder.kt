1a078f52af88d0e00c56df4d79d023ade3f8ea38
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2020-01-26 20:25:44 -0600

    Change repo structure

--- Interviews/GSTest/app/src/main/java/xyz/eliothmonroy/gs_test/util/common/data/service/BaseServiceBuilder.kt
+++ Interviews/GSTest/app/src/main/java/xyz/eliothmonroy/gs_test/util/common/data/service/BaseServiceBuilder.kt
@@ -1,23 +1,15 @@
 package xyz.eliothmonroy.gs_test.util.common.data.service
 
-import com.google.gson.FieldNamingPolicy
-import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object BaseServiceBuilder {
-    private var retrofitService:Retrofit
-    init {
-        val gson=GsonBuilder()
-            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-            .create()
-        retrofitService=Retrofit.Builder()
-            .baseUrl(ConstantsCommons.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-            .build()
-    }
+    private var retrofitService:Retrofit = Retrofit.Builder()
+        .baseUrl(ConstantsCommons.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+        .build()
 
     fun getRetrofitInstance():Retrofit{
         return retrofitService
