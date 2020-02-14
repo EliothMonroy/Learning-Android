298b25cf0c46e3b58b19645d7fb6ba4d5f00b99a
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2020-02-14 14:05:14 -0600

    Add new response wrapper for retrofit petition

--- Libraries/TestLibraryImplementation/app/src/main/java/xyz/eliothmonroy/testlibraryimplementation/MainActivity.kt
+++ Libraries/TestLibraryImplementation/app/src/main/java/xyz/eliothmonroy/testlibraryimplementation/MainActivity.kt
@@ -2,14 +2,30 @@ package xyz.eliothmonroy.testlibraryimplementation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import xyz.eliothmonroy.testlibrary.data.model.Response
+import xyz.eliothmonroy.testlibraryimplementation.presentation.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel:MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
+        showTodo()
 
     }
+
+    fun showTodo(){
+        mainViewModel.todo.observe(this, Observer {response->
+            Log.d("Main: ",response.toString())
+        })
+    }
+
 }
