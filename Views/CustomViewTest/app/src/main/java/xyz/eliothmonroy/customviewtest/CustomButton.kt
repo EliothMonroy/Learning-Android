1a0d64a4f38322fe0d085961e5ac77376e641a5f
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2020-02-19 18:55:08 -0600

    Add custom styles

--- Views/CustomViewTest/app/src/main/java/xyz/eliothmonroy/customviewtest/CustomButton.kt
+++ Views/CustomViewTest/app/src/main/java/xyz/eliothmonroy/customviewtest/CustomButton.kt
@@ -5,26 +5,15 @@ import android.graphics.Color
 import android.util.AttributeSet
 import android.widget.Button
 import androidx.appcompat.view.ContextThemeWrapper
+import androidx.appcompat.widget.AppCompatButton
 
 
-class CustomButton(context: Context, st:AttributeSet) : Button(
-    ContextThemeWrapper(
-        context,
-        //androidx.appcompat.R.style.Widget_AppCompat_Button_Colored
-        R.style.CustomButton
-    ),
-    st,
-    0
-) {
+class CustomButton(context: Context, st:AttributeSet) : AppCompatButton(ContextThemeWrapper(context, R.style.CustomButton), st, 0) {
 
     init {
-        //setBackgroundColor(Color.RED)
-        setTextColor(Color.WHITE)
-        text = "Login"
         isClickable=true
         isEnabled=true
-        //TODO: No se ve el color en android 5
-        //background.setTint(Color.RED)
+
     }
 
 }
